3afccbe16ab09105847f600ac7bee931f6d2352f
root <root@ubuntu.localdomain>
2021-08-07 06:09:29 +0000

    Added line to check the jenkin intigration with pool scm

--- Java_Helloworld.java
+++ Java_Helloworld.java
@@ -4,6 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Himamvalli welcome to world!"); 
         System.out.println("Hello, Himamvalli Added second line!"); 
+        System.out.println("Hello, This line added for check pool scm in GIT"); 
         
     }
 }
