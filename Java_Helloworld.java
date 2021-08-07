ec37c9fa516f04db0737eea6b6840123da19a465
Himamvalli Shaik <hishaik@ubuntu.localdomain>
2021-08-07 06:19:16 +0000

    second commit to check pool scm once again

--- Java_Helloworld.java
+++ Java_Helloworld.java
@@ -5,6 +5,7 @@ class HelloWorld {
         System.out.println("Hello, Himamvalli welcome to world!"); 
         System.out.println("Hello, Himamvalli Added second line!"); 
         System.out.println("Hello, This line added for check pool scm in GIT"); 
+        System.out.println("pool scm working as expected"); 
         
     }
 }
